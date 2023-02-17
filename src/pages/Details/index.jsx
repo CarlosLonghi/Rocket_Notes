80bcdd402886c74e55e791ef3affe334810ae6b1
carlos <carloslonghi.cl@gmail.com>
2023-02-17 12:06:52 -0300

    style: create component Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,17 +1,13 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details(){
   return (
     <Container>
-      <h1>Olá mundo!</h1>
-      <span>Cadu Dev</span>
-
-      <Button title='Login' loading/>
-      <Button title='Cadastrar'/>
+      <Header/>
       <Button title='Voltar'/>
-
     </Container>
   )
 }
