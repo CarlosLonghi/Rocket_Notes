f59f0f5fff406a93a05d168be1e5a1b73ccc00e5
carlos <carloslonghi.cl@gmail.com>
2023-02-17 11:41:23 -0300

    style: create component Button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
-import { Container } from './styles.js'
+import { Container } from './styles'
+
+import { Button } from '../../components/Button'
 
 export function Details(){
   return (
     <Container>
       <h1>Olá mundo!</h1>
       <span>Cadu Dev</span>
+
+      <Button />
     </Container>
   )
 }
