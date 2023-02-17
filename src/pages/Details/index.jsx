8b13e3fc8f66df87246b43ea0db7b3146ebb08fa
carlos <carloslonghi.cl@gmail.com>
2023-02-17 10:32:24 -0300

    style: add styled components in aplication

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import './styles.css'
+import { Container } from './styles.js'
 
 export function Details(){
   return (
-    <>
+    <Container>
       <h1>Olá mundo!</h1>
       <span>Cadu Dev</span>
-    </>
+    </Container>
   )
 }
