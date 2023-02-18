296c1180972cbf7903ca9fbb4cac3e329811ce9e
carlos <carloslonghi.cl@gmail.com>
2023-02-18 12:01:42 -0300

    style: NewNote style created

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -61,5 +61,15 @@ export const Content = styled.div`
 
 export const NewNote = styled.button`
   grid-area: newnote;
-  
+
+  border: none;
+  background-color: ${({theme}) => theme.COLORS.ORANGE};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    margin-right: 8px;
+  }
 `
