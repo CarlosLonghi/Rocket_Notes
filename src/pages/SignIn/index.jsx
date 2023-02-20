8bdb491ba73db242860762a40e35b9117a0bd1cd
carlos <carloslonghi.cl@gmail.com>
2023-02-20 17:43:11 -0300

    style: create styles in Page SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,33 @@
-import { Container } from "./styles";
+import { FiMail, FiLock} from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from "./styles";
 
 export function SingIn() {
   return (
     <Container>
-      
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar os seus links úteis</p>
+
+        <h2>Faça seu Login</h2>
+
+        <Input
+          placeholder='E-mail'
+          type='text'
+          icon={FiMail}
+        />
+        <Input
+          placeholder='Senha'
+          type='password'
+          icon={FiLock}
+        />
+        <Button title='Entrar'/>
+
+        <a href="#">Criar conta</a>
+      </Form>
     </Container>
   )
 }
