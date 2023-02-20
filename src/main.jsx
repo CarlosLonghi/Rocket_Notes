3c0d620fbf4fc116559c521caa77854a808d34cc
carlos <carloslonghi.cl@gmail.com>
2023-02-20 17:07:31 -0300

    feat: create Page SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { Home } from './pages/Home'
+
+import { SingIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home/>
+      <SingIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
