03df128217f99ee9b533b00d15dd538e608e58a3
carlos <carloslonghi.cl@gmail.com>
2023-02-21 10:44:49 -0300

    style: add backgound img in SignIn Page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,7 @@ import { FiMail, FiLock} from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Container, Form } from "./styles";
+import { Container, Form, Background } from "./styles";
 
 export function SingIn() {
   return (
@@ -28,6 +28,9 @@ export function SingIn() {
 
         <a href="#">Criar conta</a>
       </Form>
+
+      <Background/>
+      
     </Container>
   )
 }
