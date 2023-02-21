d40ed6546f62b561d63c2ae426680ea84e97eb42
carlos <carloslonghi.cl@gmail.com>
2023-02-21 16:50:50 -0300

    style: Profile page finish

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
     align-items: center;
     padding: 0 120px;
 
-    background: ${({theme}) => theme.COLORS.BACKGROUND_900};
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
     svg {
       color: ${({ theme }) => theme.COLORS.GRAY_100};
@@ -28,8 +28,52 @@ export const Container = styled.div`
 
 export const Form = styled.form`
   max-width: 350px;
-
   margin: 30px auto 0;
 
-  
+  > div:nth-child(3) {
+    margin-bottom: 25px;
+  }
+`
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -120px auto 64px;
+
+  width: 185px;
+  height: 185px;
+
+  > img {
+    width: 185px;
+    height: 185px;
+    border-radius: 50%;
+    box-shadow: 5px 2px 5px black;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
 `
