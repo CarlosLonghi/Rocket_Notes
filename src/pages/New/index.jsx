f8972ddfde89c69e007dd1c8b7f0c4787cfe89e6
carlos <carloslonghi.cl@gmail.com>
2023-02-22 12:39:44 -0300

    style: NoteItem styles

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header'
 import { Input} from '../../components/Input'
 import { TextArea } from '../../components/TextArea'
 import { NoteItem } from '../../components/NoteItem';
+import { Section} from '../../components/Section'
 
 import { Container, Form } from "./styles";
 
@@ -17,13 +18,14 @@ export function New() {
             <a href="/">voltar</a>
           </header>
 
-          <Input
-            placeholder='Titulo'
-          />
-          <TextArea
-            placeholder='Observações'
-          />
+          <Input placeholder='Titulo'/>
+          <TextArea placeholder='Observações'/>
 
+          <Section title='Links úteis'>
+            <NoteItem value='https://github.com.br'/>
+            <NoteItem placeholder='Novo link' isNew/>
+          </Section>
+          
         </Form>
       </main>
     </Container>
