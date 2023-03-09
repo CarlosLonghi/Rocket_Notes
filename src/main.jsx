34f4edbb228f5d00e44eda4988884114d036d479
carlos <carloslonghi.cl@gmail.com>
2023-03-09 17:34:36 -0300

    feat: create context in aplication

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from './styles/global'
-import theme from './styles/theme'
-
+import { MyContext } from './myContext'
 import { Routes } from './routes'
+
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Routes/>
+        <MyContext.Provider value={{name:'carlos', email: 'carlos@email.com'}}>
+          <Routes/>
+        </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
