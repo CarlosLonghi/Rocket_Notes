d756e7079c45b7b169e50227432df425143c2708
carlos <carloslonghi.cl@gmail.com>
2023-03-13 18:19:27 -0300

    feat: create update datas in Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -10,11 +10,23 @@ import { Button } from '../../components/Button'
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
-  const { user } = useAuth()
+  const { user, updateProfile } = useAuth()
+
   const [name, setName] = useState(user.name)
   const [email, setEmail] = useState(user.email)
-  const [passwordCurrent, setPasswordCurrent] = useState()
+  const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate(){
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+    await updateProfile({ user })
+  }
 
   return (
     <Container>
@@ -60,7 +72,7 @@ export function Profile() {
           placeholder='Senha Atual'
           type='password'
           icon={FiLock}
-          onChange={event => setPasswordCurrent(event.target.value)}
+          onChange={event => setPasswordOld(event.target.value)}
         />
         <Input
           placeholder='Nova Senha'
@@ -71,6 +83,7 @@ export function Profile() {
 
         <Button
           title='Salvar'
+          onClick={handleUpdate}
         />
       </Form>
     </Container>
