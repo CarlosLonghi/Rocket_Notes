e85062c660a09050aa97ad5c2d9322365dad30be
carlos <carloslonghi.cl@gmail.com>
2023-03-14 12:22:54 -0300

    feat: create add and remove Tags in New note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New() {
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState('')
 
+  const [tags, setTags] = useState([])
+  const [newTag, setNewTag] = useState('')
+
   function handleAddLink(){
     setLinks(prevState => [...prevState, newLink])
     setNewLink('')
@@ -21,7 +24,15 @@ export function New() {
 
   function handleRemoveLink(deleted){
     setLinks(prevState => prevState.filter(link => link !== deleted))
+  }
 
+  function handleAddTag(){
+    setTags(prevState => [...prevState, newTag])
+    setNewTag('')
+  }
+
+  function handleRemoveTag(deleted){
+    setTags(prevState => prevState.filter(tag => tag !== deleted))
   }
 
   return (
@@ -60,8 +71,23 @@ export function New() {
           
           <Section title='Tags'>
             <div className='tags'>
-              <NoteItem value='React'/>
-              <NoteItem placeholder='Nova Tag' isNew/>
+              {
+                tags.map(( tag, index ) => (
+                  <NoteItem 
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))
+              }
+
+              <NoteItem 
+                isNew
+                placeholder='Nova Tag' 
+                onChange={event => setNewTag(event.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
